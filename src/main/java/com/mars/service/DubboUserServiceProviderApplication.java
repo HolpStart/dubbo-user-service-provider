a7814af85292ec8e1d3015826f70211bcc21bdf5
qihailiang <1607855394@qq.com>
2022-03-15 11:25:24 +0800

    Initial commit

---
+++ src/main/java/com/mars/service/DubboUserServiceProviderApplication.java
@@ -0,0 +1,13 @@
+package com.mars.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboUserServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboUserServiceProviderApplication.class, args);
+    }
+
+}
